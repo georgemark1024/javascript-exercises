f03c897f07d088089169ec3383b88d81aa764534
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-07-25 20:07:28 +0300

    feat: add leapYears logic

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divBy100 = (year % 100 === 0);
 
+    if (divBy100)
+        return year % 400 === 0;
+    
+    return (year % 4 === 0);
 };
 
 // Do not edit below this line
