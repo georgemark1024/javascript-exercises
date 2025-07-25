1a677a67f1869f81d1f65abff444867de4dc7a2a
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-07-25 21:24:46 +0300

    feat: add temp conversion logic

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFht) {
+    res = (tempInFht - 32) * (5/9);
+
+    if (Math.trunc(res) == res)
+        return Math.trunc(res);
+    return parseFloat(res.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInClc) {
+    res = tempInClc * (9/5) + 32;
+
+    if (Math.trunc(res) == res)
+        return Math.trunc(res);
+    return parseFloat(res.toFixed(1));
 };
 
 // Do not edit below this line
