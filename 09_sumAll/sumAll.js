65d4be5791e240ee07f5a6b5214375fb9614d6b4
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-07-25 20:00:25 +0300

    feat: add sumAll logic

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b))
+        return 'ERROR';
+    if (a < 0 || b < 0)
+        return 'ERROR';
 
+    sum = 0;
+    a > b ? [int1, int2] = [b, a] : [int1, int2] = [a, b];
+    
+    for (i = int1; i <= int2; i++)
+        sum += i;
+    
+    return sum;
 };
 
 // Do not edit below this line
