13c12b2712c1a986ae200af2e3de7e1d57e850e0
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-07-25 17:44:48 +0300

    feat: add repeatString logic

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    strArr = [];
+    for (let i = 0; i < num; i++) {
+        strArr.push(str);
+    }
+    return strArr.join("");
 };
 
 // Do not edit below this line
