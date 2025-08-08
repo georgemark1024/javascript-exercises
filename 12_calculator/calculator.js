cc12776d2869c9e3662a57f27a0f26dec7fdf334
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-08 08:05:19 +0300

    feat: add calculator logic
    uses recursion for factorial

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(nums) {
+  let sum = 0;
+	for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+};
+
+
+const multiply = function(nums) {
+  let res = 1;
+  for (let i = 0; i < nums.length; i++) {
+    res *= nums[i];
+  }
+  return res;
+};
+
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(a) {
+  if (a == 1 || a == 0)
+    return 1;
 
-const sum = function() {
-	
-};
+  let res = a * factorial(a - 1);
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  return res;
 };
 
 // Do not edit below this line
