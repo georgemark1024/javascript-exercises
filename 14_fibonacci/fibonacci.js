4e6e9c7e2de981433ae95366318e12af72aa03e4
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-09 09:58:17 +0300

    feat: add fibonacci logic

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (num < 0)
+        return "OOPS";
+    
+    let a = 0;
+    let b = 1;
+    let fibs = [];
+    let next;
+    for (let i = 0; i <= num; i++) {
+        if (i == 0) {    
+            next = a;
+        }
+        else if (i == 1) {
+            next = b;
+        }
+        else {
+            next = a + b;
+            a = b;
+            b = next;
+        }
+        fibs.push(next);
+    }
+
+    return fibs[num];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
