a9cacf4ab8d475b810d5de0a044969e65835c685
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-10 14:49:28 +0300

    feat: add getTheTitles logic

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArr) {
+    titles = [];
+    booksArr.forEach(book => {
+        titles.push(book.title);
+    });
 
+    return titles;
 };
 
 // Do not edit below this line
