bcd2574135d88558ef57821a159fb9c83acde81f
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-10 15:40:35 +0300

    feat: add findTheOldest logic

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    let peopleWithAges = [];
 
+    peopleArr.forEach(person => {
+        if (person.yearOfBirth && person.yearOfDeath)
+            var age = person.yearOfDeath - person.yearOfBirth;
+        else {
+            const currentYear = new Date().getFullYear();
+            var age = currentYear - person.yearOfBirth;
+        }
+        
+        person.age = age;
+        peopleWithAges.push(person);
+
+
+    });
+
+    let oldest = peopleWithAges[0];
+
+    for (let i = 1; i < peopleWithAges.length; i++) {
+        if (peopleWithAges[i].age > oldest.age)
+            oldest = peopleWithAges[i];
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
