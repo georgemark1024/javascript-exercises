bdc43c4e57c67b2a80b4b934d3ba3ac1bef5566c
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-11 06:57:33 +0300

    feat: add palindromes logic

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    arr = str.split("");
+    reversedArr = [...arr].reverse();
 
+    punctuation = [",", ".", "!", " "];
+
+    // return arr === reversedArr  ;
+    let reverseIndex = 0;
+    for(let i = 0; i < arr.length, reverseIndex < reversedArr.length; i++, reverseIndex++) {
+        INNERLOOP:
+        while (1) {
+            if (punctuation.includes(arr.at(i) )) {
+                i++;
+                continue;
+            }
+            if (punctuation.includes(reversedArr.at(reverseIndex))) {
+                reverseIndex++;
+                continue;
+            }
+            break INNERLOOP;
+        }
+
+        if (arr.at(i).toLowerCase() != reversedArr.at(reverseIndex).toLowerCase())
+            return false;
+    }
+    return true;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
